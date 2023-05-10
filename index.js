ea06dcd308a7d80becde01c79e8d2291f96fec69
wms09291963 <98226872+wms09291963@users.noreply.github.com>
2023-05-10 09:53:48 -0400

    Updated routes to show query params

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+//require needed modules 
+const express = require('express')
+const app=express()
+
+// Declare routes that people can visit on the application.
+app.get('/', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+})
+
+
+// Declare routes that people can visit on the application.
+// Teal Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
+})
+
+// Crimson Page
+
+
+
+
+app.listen (process.env.PORT)
